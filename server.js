a8e691f8dfb5ce0c408e25b2e9be6449dd7ecb7d
virajsolanki5057-ui <viraj.solanki.5057@gmail.com>
2026-05-14 10:47:56 +0530

    fix error in app.js

--- server.js
+++ server.js
@@ -1,21 +1,17 @@
+import app from "./src/app.js";
 import mongoose from "mongoose";
-import app from "./app.js";
+import "dotenv/config";
 
 const PORT = process.env.PORT || 5000;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
     console.log("✅ MongoDB Connected");
-
     app.listen(PORT, () => {
       console.log(`🚀 Server running on port ${PORT}`);
     });
-
-  } catch (error) {
-    console.error("❌ MongoDB Connection Failed:", error.message);
-    process.exit(1);
-  }
-};
-
-connectDB();
+  })
+  .catch((err) => {
+    console.error("❌ DB Error:", err);
+  });
